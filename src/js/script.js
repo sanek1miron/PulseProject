ad88af618f86d42598e79e8626722d8f9096245d
Aleksandr <sanek1miron@gmail.com>
2024-10-19 23:47:22 +0300

    add footer

---
+++ src/js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+      speed: 1200,
+      prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right.svg"></button>',
+      responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              dots: true,
+              arrows: false,
+            }
+          },
+        ]
+  });
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog_content').removeClass('catalog_content_active').eq($(this).index()).addClass('catalog_content_active');
+  });
+
+  // $('.catalog-item__link').each(function(i) {
+  //   $(this).on('click', function(e) {
+  //     e.preventDefault();
+  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  //   });
+  // });
+
+  // $('.catalog-item__back').each(function(i) {
+  //   $(this).on('click', function(e) {
+  //     e.preventDefault();
+  //     $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+  //     $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  //   });
+  // });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+      $(this).on('click', function(e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      });
+    });
+  };
+
+  toggleSlide('.catalog-item__link');
+
+  toggleSlide('.catalog-item__back');
+
+});
